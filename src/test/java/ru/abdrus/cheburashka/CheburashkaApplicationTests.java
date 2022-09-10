2f31eaddf95032925d1a2d52dbdf26c739dbb0ed
abdrakhmanovrr <r.abdrakhmanov@haulmont.com>
2022-09-10 22:10:55 +0400

    init-commit

---
+++ src/test/java/ru/abdrus/cheburashka/CheburashkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abdrus.cheburashka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheburashkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
